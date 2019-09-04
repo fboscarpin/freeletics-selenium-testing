83ff5dbb78edc77107fec94438f1c87ea4c590e7
Jason Franklin <jason.franklin@xing.com>
2019-09-04 23:52:11 +0200

    Add Android configuration

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,26 @@
 require 'selenium-webdriver'
+require 'appium_lib'
+
+desired_caps = {
+    caps:  {
+        platformName:  'Android',
+        platformVersion: '9',
+        deviceName:    'emulator-5554',
+        browserName:   'Chrome',
+    }
+}
+
+# Android driver
+$appium_driver = Appium::Driver.new(desired_caps, true)
+$driver = $appium_driver.start_driver
+Appium.promote_appium_methods Object
 
 #Instance the driver
-Selenium::WebDriver::Chrome::Service.driver_path='/usr/local/bin/chromedriver'
-options = Selenium::WebDriver::Chrome::Options.new
-$driver = Selenium::WebDriver.for :chrome, options: options
-$driver.manage.window.maximize
-$driver.manage.timeouts.implicit_wait = 10 #seconds
+# Selenium::WebDriver::Chrome::Service.driver_path='/usr/local/bin/chromedriver'
+# options = Selenium::WebDriver::Chrome::Options.new
+# $driver = Selenium::WebDriver.for :chrome, options: options
+# $driver.manage.window.maximize
+# $driver.manage.timeouts.implicit_wait = 10 #seconds
 
 #Hook
 After ('@CloseTest') do
